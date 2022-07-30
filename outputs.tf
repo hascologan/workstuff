18fea50133ba306e44ef9ac49e8a786559500215
halo28 <83364764+hascologan@users.noreply.github.com>
2022-07-30 09:08:47 -0700

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tf_statefile_s3" {
-  value       = aws_s3_bucket.tf_statefile_s3.bucket
-  description = "The name of the S3 bucket to use for Terraform statefiles"
-}
-
-output "tf_lock_status_DDB" {
-  value       = aws_dynamodb_table.tf_lock_status_ddb.id
-  description = "The name of the DynamoDB table used for Terraform lock status"
-}
